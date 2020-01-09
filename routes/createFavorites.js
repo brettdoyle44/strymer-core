cf1f0b946cfafe0f65b5146d98efa78aa9aec615
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2020-01-09 17:01:59 -0500

    Update create to async function

--- routes/createFavorites.js
+++ routes/createFavorites.js
@@ -1,7 +1,7 @@
 import * as dynamoDbLib from '../libs/dynamodb-lib';
 import { success, failure } from '../libs/response-lib';
 
-export function main(event, context) {
+export async function main(event, context) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -14,7 +14,7 @@ export function main(event, context) {
   };
 
   try {
-    await dynamoDbLib.call("put", params);
+    await dynamoDbLib.call('put', params);
     return success(params.Item);
   } catch (e) {
     return failure({ status: false });
